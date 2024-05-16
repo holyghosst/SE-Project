02af73a4b0e26ed93cfdc2fc6eba9373fd6d1133
Oleksandr Lymarenko <a12028261@univie.ac.at>
2024-05-16 17:20:54 +0200

    added output functionsfor the changed properties

--- Source/Teilaufgabe 2 - Client/ExampleClient/src/main/java/clientMVC/GameStateView.java
+++ Source/Teilaufgabe 2 - Client/ExampleClient/src/main/java/clientMVC/GameStateView.java
@@ -4,6 +4,7 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 import map.ClientFullMap;
+import map.HalfMap;
 import network.ClientPlayerState;
 
 public class GameStateView implements PropertyChangeListener {
@@ -12,18 +13,74 @@ public class GameStateView implements PropertyChangeListener {
 	gameState.addPropertyChangeListener(this);
     }
 
+    public void printOutHalfMap(HalfMap halfMap) {
+	System.out.println();
+	StringBuilder stringBuilder = new StringBuilder();
+	for (int y = 0; y < 5; y++) {
+	    stringBuilder.setLength(0);
+	    for (int x = 0; x < 10; x++) {
+		stringBuilder.append(halfMap.getField(x, y).printOut());
+	    }
+	    System.out.println(stringBuilder.toString());
+	}
+    }
+
+    public void printOutFullMap(ClientFullMap map) {
+	System.out.println();
+	StringBuilder stringBuilder = new StringBuilder();
+	for (int y = 0; y <= map.getMaximalY(); y++) {
+	    stringBuilder.setLength(0);
+	    for (int x = 0; x <= map.getMaximalX(); x++) {
+		stringBuilder.append(map.getField(x, y).printOut());
+	    }
+	    System.out.println(stringBuilder.toString());
+	}
+
+    }
+
+    public String printOutGameStatus(ClientPlayerState playerState) {
+	switch (playerState.getState()) {
+	case Lost:
+	    return "client has lost.";
+	case Won:
+	    return "client has won.";
+	case MustAct:
+	    return "client must act.";
+	case MustWait:
+	    return "client must wait.";
+	default:
+	    return "Unknown.";
+	}
+    }
+
+    public void printOutPlayerState(ClientPlayerState playerState) {
+	if (playerState.hasCollectedTreasure()) {
+	    System.out.print("Treasure collected, ");
+	} else {
+	    System.out.print("Treasure NOT collected, ");
+	}
+	System.out.print(printOutGameStatus(playerState));
+	System.out.println();
+    }
+
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
 	if (evt.getPropertyName().equals("map")) {
 	    if (evt.getNewValue() instanceof ClientFullMap) {
 		ClientFullMap map = (ClientFullMap) evt.getNewValue();
-		map.printOut();
+		printOutFullMap(map);
 	    }
 	}
 	if (evt.getPropertyName().equals("currentPlayerState")) {
 	    if (evt.getNewValue() instanceof ClientPlayerState) {
 		ClientPlayerState playerState = (ClientPlayerState) evt.getNewValue();
-		playerState.printOut();
+		printOutPlayerState(playerState);
+	    }
+	}
+	if (evt.getPropertyName().equals("halfmap")) {
+	    if (evt.getNewValue() instanceof HalfMap) {
+		HalfMap halfMap = (HalfMap) evt.getNewValue();
+		printOutHalfMap(halfMap);
 	    }
 	}
     }
