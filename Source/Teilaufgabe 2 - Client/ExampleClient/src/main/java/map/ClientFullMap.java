3cefc45f8ebd68f74fab2d24d855dfadaf9e31fd
Oleksandr Lymarenko <a12028261@univie.ac.at>
2024-05-16 17:06:48 +0200

    refactored ClientFullMap

--- Source/Teilaufgabe 2 - Client/ExampleClient/src/main/java/map/ClientFullMap.java
+++ Source/Teilaufgabe 2 - Client/ExampleClient/src/main/java/map/ClientFullMap.java
@@ -4,10 +4,15 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ClientFullMap extends AbstractMap {
-    private static int maximalX = 0;
-    private static int maximalY = 0;
+public class ClientFullMap {
+    private int maximalX = 0;
+    private int maximalY = 0;
+    private PartialMap myHalf;
+    private PartialMap enemyHalf;
+
     private Map<Coordinates, MapField> fields = new HashMap<Coordinates, MapField>();
+    private Coordinates startPosition;
+    private Coordinates currentPosition;
 
     private void setMaxCoordinates(Coordinates coordinates) {
 	if (coordinates.getX() > maximalX) {
@@ -18,34 +23,83 @@ public class ClientFullMap extends AbstractMap {
 	}
     }
 
+    public int getMaximalX() {
+	return maximalX;
+    }
+
+    public int getMaximalY() {
+	return maximalY;
+    }
+
     public MapField getField(int xPosition, int yPosition) {
 	return fields.get(new Coordinates(xPosition, yPosition));
     }
 
+    public MapField getField(Coordinates coordinates) {
+	return fields.get(coordinates);
+    }
+
+    public Coordinates getCurrentPosition() {
+	return currentPosition;
+    }
+
+    public Coordinates getStartPosition() {
+	return startPosition;
+    }
+
+    public void findMapHalves() {
+
+	int xMaxForVerticalMap = 9, yMaxForVerticalMap = 9, horizontalMapDivider = 9;
+	int verticalMapDivider = 4;
+
+	if (maximalX == xMaxForVerticalMap) {
+	    if (startPosition.getY() <= verticalMapDivider) {
+		myHalf = new PartialMap(0, maximalX, 0, verticalMapDivider);
+		enemyHalf = new PartialMap(0, maximalX, verticalMapDivider + 1, yMaxForVerticalMap);
+
+	    } else {
+		myHalf = new PartialMap(0, maximalX, verticalMapDivider + 1, yMaxForVerticalMap);
+		enemyHalf = new PartialMap(0, maximalX, 0, verticalMapDivider);
+	    }
+	} else {
+	    if (startPosition.getX() < horizontalMapDivider) {
+		myHalf = new PartialMap(0, horizontalMapDivider, 0, maximalY);
+		enemyHalf = new PartialMap(horizontalMapDivider, maximalX, 0, maximalY);
+	    } else {
+		myHalf = new PartialMap(horizontalMapDivider, maximalX, 0, maximalY);
+		enemyHalf = new PartialMap(0, horizontalMapDivider, 0, maximalY);
+	    }
+	}
+    }
+
+    public PartialMap getMyHalf() {
+	return myHalf;
+    }
+
+    public PartialMap getEnemyHalf() {
+	return enemyHalf;
+    }
+
+    public boolean onEnemyTerritory() {
+	int xCurrent = currentPosition.getX();
+	int yCurrent = currentPosition.getY();
+	return (xCurrent >= enemyHalf.getxMin() && xCurrent <= enemyHalf.getxMax() && yCurrent >= enemyHalf.getyMin()
+		&& yCurrent <= enemyHalf.getyMax());
+    }
+
     public void addField(Coordinates coordinates, MapField field) {
 	setMaxCoordinates(coordinates);
+	if (field.playerPresent()) {
+	    currentPosition = coordinates;
+	}
+	if (field.hasFortress()) {
+	    startPosition = coordinates;
+	}
 	fields.put(coordinates, field);
     }
 
     public Map<Coordinates, MapField> getFields() {
 	return Collections.unmodifiableMap(fields);
-    }
-
-    public void printOut() {
-	StringBuilder stringBuilder = new StringBuilder();
-	for (int y = 0; y < maximalY + 1; y++) {
-	    for (int x = 0; x < maximalX + 1; x++) {
-		stringBuilder.append(getField(x, y).toString());
-	    }
-	}
-	String mapString = stringBuilder.toString();
-	for (int i = 0; i < mapString.length(); i++) {
-	    if (i % (maximalX + 1) == 0 && i != 0) {
-		System.out.println();
-	    }
-	    System.out.print(mapString.charAt(i));
-	}
-	System.out.println();
     }
 
 }
