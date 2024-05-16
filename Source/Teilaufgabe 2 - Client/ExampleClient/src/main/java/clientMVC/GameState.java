0fe0ea9d197e01374a35af6c434b6ce1790dcbbb
Oleksandr Lymarenko <a12028261@univie.ac.at>
2024-05-16 17:19:56 +0200

    refactored gameState

--- Source/Teilaufgabe 2 - Client/ExampleClient/src/main/java/clientMVC/GameState.java
+++ Source/Teilaufgabe 2 - Client/ExampleClient/src/main/java/clientMVC/GameState.java
@@ -4,16 +4,30 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 import map.ClientFullMap;
+import map.HalfMap;
+import network.ClientPlayerGameState;
 import network.ClientPlayerState;
 
 public class GameState {
+    private HalfMap halfMap;
     private ClientPlayerState currentPlayerState;
     private ClientFullMap map;
+    private StateOfGame currentStateOfGame;
+
+    public GameState() {
+	currentStateOfGame = StateOfGame.UNDETERMINED;
+    }
 
     private final PropertyChangeSupport changes = new PropertyChangeSupport(this);
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
 	changes.addPropertyChangeListener(listener);
+    }
+
+    public void setHalfMap(HalfMap halfMap) {
+	HalfMap halfMapBeforeChange = this.halfMap;
+	this.halfMap = halfMap;
+	changes.firePropertyChange("halfmap", halfMapBeforeChange, halfMap);
     }
 
     public void setMap(ClientFullMap map) {
@@ -22,10 +36,32 @@ public class GameState {
 	changes.firePropertyChange("map", mapBeforeChange, map);
     }
 
+    private void setCurrentStateOfGame(ClientPlayerState currentPlayerState) {
+	if (currentPlayerState.getState() == ClientPlayerGameState.MustAct
+		|| currentPlayerState.getState() == ClientPlayerGameState.MustWait) {
+	    currentStateOfGame = StateOfGame.UNDETERMINED;
+	} else {
+	    currentStateOfGame = StateOfGame.ENDED;
+	}
+    }
+
+    public StateOfGame getCurrentStateOfGame() {
+	return currentStateOfGame;
+    }
+
     public void setCurrentPlayerState(ClientPlayerState playerState) {
 	ClientPlayerState stateBeforeChange = this.currentPlayerState;
 	this.currentPlayerState = playerState;
+	setCurrentStateOfGame(playerState);
 	changes.firePropertyChange("currentPlayerState", stateBeforeChange, playerState);
+    }
+
+    public ClientPlayerState getCurrentPlayerState() {
+	return currentPlayerState;
+    }
+
+    public ClientFullMap getMap() {
+	return map;
     }
 
 }
